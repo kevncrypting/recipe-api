2f3785638fae38bff38cd40a540f01a93702e733
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-17 10:22:44 -0400

    Adds heavy commenting to app.js file explaining start-up process

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ app.use('/', indexRouter);
 app.use('/recipes', recipesRouter); // middleware that redirects users to use the recipesRouter route when they go to /recipes
 
 module.exports = app;
+
+/* Steps I took to start the API:
+
+1. From your workspace, use the command: 'npx express-generator --no-view <name-of-your-app>'
+
+    Replace <name-of-your-app> with your planned API name (making sure to use dashes instead of spaces if needed). This will create your boilerplate routes file structure.
+
+2. Following the instructions listed from express-generator that were printed into your terminal after install, navigate into the newly-created directory and run the command: 'npm install' to install all the dependencies. 
+
+2a. Optional: For development purposes, I installed the 'nodemon' package with the commend: 'npm install nodemon -D'
+
+    This installs nodemon as a developer dependency, meaning if a user were to fork and clone down my API and run npm install, it won't be added into their 'node_modules' file as it was only needed for development. 
+
+    If you do this step, go into your package.json file and modify the 'start' command under 'scripts' to say 'nodemon' instead of 'node' - now when you do npm start, nodemon automatically refreshes your server when you make changes
+
+3. I created a '.gitignore' file and added 'node_modules' so that it wouldn't get pushed with the rest of the files to GitHub
+
+4. I created a git repository and went through the usual steps to initialize the directory, add, commit, and push
+
+*/
